8d4f8aa1543483b94c1bfe48f5fd274d28681dbf
Vishal Birajdar <vishalbirajdar7030@gmail.com>
2025-03-02 12:54:42 +0530

    Production

--- src/swagger.js
+++ src/swagger.js
@@ -5,11 +5,11 @@ const swaggerOptions = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "User Registration API",
+            title: "Bitcoin API",
             version: "1.0.0",
-            description: "API documentation for user registration with OTP verification using Amazon SES and PostgreSQL",
+            description: "API documentation for user registration with OTP verification using Nodemailer SMTP and PostgreSQL",
         },
-        servers: [{ url: "http://localhost:5000" }],
+        servers: [{ url: "http://64.225.19.128:5000" }],
     },
     apis: ["./src/routes/*.js"], 
 };
